4492219d58f3e0dcf121b930cb62039fcae5c88a
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2016-08-12 10:53:18 +0200

    Added error messages.

--- src/Converter/ConverterMain.cpp
+++ src/Converter/ConverterMain.cpp
@@ -3,13 +3,22 @@
 
 #include "stdio.h"
 #include "Converter.h"
+#include <TCFoundation/TCLocalStrings.h>
+#include "LDViewMessages.h"
 
 int main(int argc, char* argv[])
 {
+    int stringTableSize = sizeof(LDViewMessages_bytes);
+    char *stringTable = new char[sizeof(LDViewMessages_bytes) + 1];
+
+    memcpy(stringTable, LDViewMessages_bytes, stringTableSize);
+    stringTable[stringTableSize] = 0;
+    TCLocalStrings::setStringTable(stringTable);
+
     Converter *converter = new Converter;
     int retValue = converter->run(argc, argv);
 
     converter->release();
+    delete [] stringTable;
     return retValue;
 }
-
